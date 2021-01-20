53f3622f7a3c0c235f7e1fb992051fb7ede28b27
haileyringier <haileyringier@outlook.com>
2021-01-20 11:13:40 -0700

    inital setup and component structure

---
+++ src/Components/PokemonCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PokemonCard() {
+    return (
+        <card>
+            <h3>Pokemon Card</h3>
+        </card>
+    )
+}
---
+++ src/Components/PokemonSection.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PokemonSection() {
+    return (
+        <div>
+            <h2>Pokemon Section</h2>
+        </div>
+    )
+}
